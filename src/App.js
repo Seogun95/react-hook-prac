9f8fb30a03e3db8995240187cf644e12c77094f0
Seogun95 <ksj083895@gmail.com>
2023-02-10 04:26:48 +0900

    feat: about useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 
 function App() {
-  return <div></div>;
+  const [num, setNum] = useState(0);
+  return (
+    <>
+      <p>{num}</p>
+      {/* 기존 업데이트 방식 */}
+      <button onClick={() => setNum(num + 1)}>카운트</button>
+      {/* //함수형 업데이트 방식 */}
+      <button onClick={() => setNum((currentState) => currentState + 1)}>카운트</button>
+    </>
+  );
 }
-
 export default App;
